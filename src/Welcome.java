afeb04fbf58d9cbcbadc7a622a012c8011f71563
NhatHao1003 <23721351.hao@student.iuh.edu.vn>
2024-08-13 20:04:10 +0700

    First commit

---
+++ src/Welcome.java
@@ -0,0 +1,6 @@
+public class Welcome {
+    public static void main(String[] args) {
+        System.out.println("Hello Nguyễn Phương Nhật Hào!");
+        System.out.println("Welcome to DHKTPM19B!");
+    }
+}
